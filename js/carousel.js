9ebb721634b50ed9850bb78db7eee92293d778ab
Stanislav Gorokh <137794935+StanislavGo@users.noreply.github.com>
2024-01-12 00:42:52 +0200

    feat: add switching effect to the carusel

--- js/carousel.js
+++ js/carousel.js
@@ -3,6 +3,8 @@ const arrowRigth = document.querySelector(".arrow-right");
 const slider = document.querySelector(".slider-block .slider .elements");
 const indicatorsArr = document.querySelectorAll(".pagination-block .pagination");
 const sliderWidth = document.querySelector(".slider-block .slider .elements .slide").clientWidth;
+let arrOfWidths = [0, -sliderWidth, -sliderWidth * 2];
+const sliderBlock = document.querySelector(".slider-block .slider");
 let clicks = 0;
 
 function restartCarusel(index, count, px) {
@@ -22,21 +24,10 @@ function changePaginationStatus(index) {
 }
 
 function goToSlide(index) {
-  const arr = [0, -sliderWidth, -sliderWidth * 2];
-  slider.style.left = arr[index] + "px";
+  slider.style.left = arrOfWidths[index] + "px";
 }
 
-arrowRigth.onclick = function() {
-  clicks++;
-  if (clicks < 3) {
-    goToSlide(clicks);
-    changePaginationStatus(clicks);
-  } else {
-    restartCarusel(0, 0, 0);
-  }
-}
-
-arrowLeft.onclick = function() {
+function goLeft() {
   clicks--;
   if(clicks == -1) {
     restartCarusel(2, 2, -sliderWidth * 2);
@@ -44,4 +35,48 @@ arrowLeft.onclick = function() {
     goToSlide(clicks);
     changePaginationStatus(clicks);
   }
+  const resetWidth = document.querySelector(".slider-block .slider .elements .slide").clientWidth;
+  arrOfWidths = [0, -resetWidth, -resetWidth * 2];
 }
+
+function goRight() {
+  clicks++;
+  if (clicks < 3) {
+    goToSlide(clicks);
+    changePaginationStatus(clicks);
+  } else {
+    restartCarusel(0, 0, 0);
+  }
+  const resetWidth = document.querySelector(".slider-block .slider .elements .slide").clientWidth;
+  arrOfWidths = [0, -resetWidth, -resetWidth * 2];
+}
+
+arrowRigth.onclick = function() {
+  goRight();
+}
+
+arrowLeft.onclick = function() {
+  goLeft();
+}
+
+let timerID;
+function runSwitchEffect() {
+  timerID = setInterval(() => {
+    goRight();
+  }, 6300)
+}
+
+sliderBlock.onmouseenter = function() {
+  clearInterval(timerID);
+}
+sliderBlock.onmousedown = function() {
+  clearInterval(timerID);
+}
+sliderBlock.onmouseleave = function() {
+  runSwitchEffect();
+}
+sliderBlock.onmouseup = function() {
+  runSwitchEffect();
+}
+
+runSwitchEffect();
